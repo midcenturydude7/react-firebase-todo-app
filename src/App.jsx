5bb84e66844110da48636a4b7bcc8a7af754586a
midCenturyDude7 <mgriffes@gmail.com>
2023-03-18 20:24:18 -0700

    Created Todo component and mapped over todo array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 import "./index.css";
+import Todo from "./Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+};
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, World!</h1>;
+  const [todos, setTodos] = React.useState(["Learn React", "Grind Leetcode"]);
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Add Todo..."
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
