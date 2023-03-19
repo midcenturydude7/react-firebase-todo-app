3c04c8209f507973c5ee00e4162ddb06016be75e
midCenturyDude7 <mgriffes@gmail.com>
2023-03-19 09:06:04 -0700

    Getting ready to set up Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const style = {
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
   button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`
+  count: `text-center p-2`,
 };
 
 function App() {
